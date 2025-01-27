1173a2bbc82918a9f5c9ae4659ca0ce4f37efc39
hkaur427 <hkaur427@flemingcollege.ca>
2025-01-27 16:58:05 -0500

    step:3
    added another movie to the data structure

---
+++ lab02.py
@@ -0,0 +1,48 @@
+def main():
+
+    # TODO: Step 2 - Create a complex data structure
+    about_me = { 
+        'full_name':'Harmanpreet kaur',
+            'student_id': '10362758',
+            'pizza_topping':
+            ['CHEESE',
+             'PINEAPPLES',
+                'CAPCICUM',]
+                }
+     'movies':[
+                'action_movies':'the dark knight',
+                'horror_name': 'halloween', 
+                'drama_movie': 'one fast move',
+                'funny_movie': 'jackpot',  
+                }
+
+    # TODO: Step 3 - Add another movie to the data structure
+    another_movie ={
+        'sad_movie':'beautiful boy in the town'
+      }
+      about_me["movies"].append(another_movie)
+      print(about_me)
+
+    
+# TODO: Step 4 - Function that prints student name and ID	
+def print_student_name_and_id(about_me):
+    return
+    
+# TODO: Step 5 - Function that adds pizza toppings to data structure
+def add_pizza_toppings(about_me, toppings):
+    return
+
+# TODO: Step 6 - Function that prints bullet list of pizza toppings
+def print_pizza_toppings(about_me):
+    return
+
+# TODO: Step 7 - Function that prints comma-separated list of movie genres
+def print_movie_genres(about_me):
+    return 
+
+# TODO: Step 8 - Function that prints comma-separated list of movie titles
+def print_movie_titles(movie_list):
+    return
+    
+if __name__ == '__main__':
+    main()
