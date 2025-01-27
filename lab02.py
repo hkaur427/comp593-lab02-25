35f5ec8655dce8d8318f24097ccb7d0b422e7a28
hkaur427 <hkaur427@flemingcollege.ca>
2025-01-27 17:49:47 -0500

    step:8
    Function that prints comma-separated list of movie titles

--- lab02.py
+++ lab02.py
@@ -53,6 +53,9 @@ def print_movie_genres(about_me):
 
 # TODO: Step 8 - Function that prints comma-separated list of movie titles
 def print_movie_titles(movie_list):
+    titles = [movie['title'].title() for movie in movie_list]
+    title_list = ', '.join(titles[:-1]) + f", and {titles[-1]}" if len(titles) > 1 else titles[0]
+    print(f"Some of my favourite movies are {title_list}!")
     return
     
 if __name__ == '__main__':
